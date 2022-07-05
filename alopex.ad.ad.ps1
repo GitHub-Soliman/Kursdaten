d1191e1a99a4cccf8a0e7fc48399b8a74687f1eb
GitHub-Soliman <GitHub@soliman.de>
2022-07-05 15:38:42 +0200

    Add files via upload

---
+++ alopex.ad.ad.ps1
@@ -0,0 +1,8 @@
+﻿Get-WindowsFeature "ser*-gui*","Web-Mgmt-Tools","wAS*","RSAT*","Ad-Dom*","Desktop-Experience","uSer*","Telnet-Client"|Install-WindowsFeature  -Verbose -IncludeAllSubFeature -IncludeManagementTools 
+man Install-ADDSForest -Online
+$admkw=read-host -Prompt "Adminkennwörtchen." -AsSecureString -Verbose
+$Domänenname="alopex"
+Import-Module ADDSDeployment -Verbose
+Install-ADDSForest  -CreateDnsDelegation:$false -DatabasePath "C:\windows\NTDS" -DomainMode "WinThreshold" -DomainName "$Domänenname.local" `
+					-DomainNetbiosName "$Domänenname" -ForestMode "WinThreshold" -InstallDns:$true -LogPath "C:\windows\NTDS" -NoRebootOnCompletion:$true `
+					-SysvolPath "C:\windows\SYSVOL" -Force:$true -Verbose -SafeModeAdministratorPassword $admkw 
